6529061790c16e9f73165a296d6fb0d43e61ddae
BarkMulcher <lucas.c.haskell@gmail.com>
2022-08-17 11:54:53 -0400

    Upload for test

---
+++ script.js
@@ -0,0 +1,46 @@
+// CATAPI:
+// CatAPI docs url: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+// Cat API key: 7b5f98ae-8857-4f17-8dd4-2e4efa503daf
+const catApiUrl = 'https://api.thecatapi.com/v1/images/search';
+var catPicBtn = document.getElementById('get-cat-button');
+var catBox = document.querySelector('.cat-box');
+window.onload = function init() {
+    
+}
+
+var getCatPicButtonHandler = function (event) {
+    event.preventDefault();
+    getRandomCat();
+}
+
+
+// get cat pic function:
+var getRandomCat = function() {
+    fetch(catApiUrl)
+    .then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            return response.json();
+        }
+    })
+    .then(function(data) {
+        console.log(data);
+        let catImgUrl = data[0].url;
+        console.log(data[0].url);
+        displayCat(data);
+    })
+
+}
+
+var displayCat = function() {
+    var catPicElement = document.createElement('img');
+    let catImgUrl = data[0].url;
+    catPicElement.setAttribute('src', catImgUrl);
+    catBox.appendChild(catPicElement);
+}
+
+const catResult = document.getElementById('cat-result');
+ 
+
+
+catPicBtn.addEventListener('click', getCatPicButtonHandler);
