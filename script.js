84618708d532cbe7e33104e49b6df3d8c85c9bd3
BarkMulcher <lucas.c.haskell@gmail.com>
2022-08-17 20:10:24 -0400

    JS and HTML update for onload & fixes

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
-// CATAPI:
-// CatAPI docs url: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
-// Cat API key: 7b5f98ae-8857-4f17-8dd4-2e4efa503daf
-const catApiUrl = 'https://api.thecatapi.com/v1/images/search';
-var catPicBtn = document.getElementById('get-cat-button');
-var catBox = document.querySelector('.cat-box');
-var catPicElement = document.createElement('img');
+$(document).ready(function () {
+    // CATAPI:
+    // CatAPI docs url: https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
+    // Cat API key: 7b5f98ae-8857-4f17-8dd4-2e4efa503daf
+    const catApiUrl = 'https://api.thecatapi.com/v1/images/search';
+    var catPicBtn = document.getElementById('get-cat-button');
+    var catBox = document.querySelector('.cat-box');
+    var catPicElement = document.createElement('img');
 
 
 
-window.onload = function init() {
-    
-}
-
-var getCatPicButtonHandler = function (event) {
-    event.preventDefault();
-    
-    catPicElement.textContent = '';
-    getRandomCat();
-}
 
 
-// get cat pic function:
-var getRandomCat = function() {
-    fetch(catApiUrl)
-    .then(function (response) {
-        if (response.ok) {
-            console.log(response);
-            return response.json();
-        }
-    })
-    .then(function(data) {
-        console.log(data);
+    var getCatPicButtonHandler = function (event) {
+        event.preventDefault();
+
+        catPicElement.textContent = '';
+        getRandomCat();
+    }
+
+
+    // get cat pic function:
+    var getRandomCat = function () {
+        fetch(catApiUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    console.log(response);
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                console.log(data);
+                let catImgUrl = data[0].url;
+                console.log(data[0].url);
+                displayCat(data);
+            })
+
+    }
+
+    var displayCat = function (data) {
         let catImgUrl = data[0].url;
-        console.log(data[0].url);
-        displayCat(data);
-    })
+        catPicElement.setAttribute('src', catImgUrl);
+        catBox.appendChild(catPicElement);
+    }
 
-}
+    var init = function () {
+        getRandomCat();
+    }
 
-var displayCat = function(data) {
-    let catImgUrl = data[0].url;
-    catPicElement.setAttribute('src', catImgUrl);
-    catBox.appendChild(catPicElement);
-}
+    catPicBtn.addEventListener('click', getCatPicButtonHandler);
 
-const catResult = document.getElementById('cat-result');
- 
+    init();
+
+})
 
 
-catPicBtn.addEventListener('click', getCatPicButtonHandler);
+
+
